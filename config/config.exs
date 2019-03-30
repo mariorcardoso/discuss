285f7d36cdf81de37918c133e1e43b85685f91c6
Mario Cardoso <m.cardoso@runtime-revolution.com>
2019-03-30 13:40:56 +0000

    configure OAuth with Github

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+  client_id: "121fdf1c4701aafffeaa",
+  client_secret: "5f8ace78661ffc0b72bdc424ef981ae5e6684920"
